429340a6eadf56070acd6eb3c325b52fc58f21a3
nothaldu <nothaldu@pm.me>
2025-06-17 03:36:04 -0400

    copy the env template if it's missing

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,13 @@ set -e
 
 cd /app
 
+if [ ! -f /app/data/.env ]; then
+    echo "Creating .env file..."
+    cp /app/.env.example /app/data/.env
+    echo "Please fill in the .env file!"
+    exit 1
+fi
+
 uv run manage.py migrate
 uv run manage.py collectstatic --noinput
 uv run daphne blendify.asgi:application -b 0.0.0.0 -p 8000
